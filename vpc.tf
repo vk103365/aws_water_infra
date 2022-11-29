98db584eb0e3c82cf3b649e28c95b3c177c4c64e
varun <varun.kaushik@aristocrat.com>
2022-11-29 16:29:45 +1100

    adding aws

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
